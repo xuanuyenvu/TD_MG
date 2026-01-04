4bfc68e4db7f5e759a3b7d698135c14bc740e3fd
Uyen Vu <117455714+xuanuyenvu@users.noreply.github.com>
2026-01-04 16:43:06 +0100

    Track large assets with Git LFS

---
+++ Assets/Script/NavigationScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationScript : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
